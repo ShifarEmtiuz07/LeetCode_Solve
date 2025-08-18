338afab7546fb4f35728ac06bdedbe441faff26c
A. B. M. Shifar Emtiuz <90374362+ShifarEmtiuz07@users.noreply.github.com>
2025-08-18 11:39:27 +0600

    Time: 0 ms (100%), Space: 18.5 MB (78.73%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -5,35 +5,61 @@ class Solution {
 public:
  
     void setZeroes(vector<vector<int>>& m) {
-        int r = m.size();
-        int c = m[0].size();
+        int row = m.size();
+        int col = m[0].size();
 
         
-        vector<vector<int>> ans = m;
+        // vector<vector<int>> ans = m;
 
        
 
-           for(int i=0;i<r;i++){
-            for(int j=0;j<c;j++){
-              if(m[i][j]==0){
+        //    for(int i=0;i<r;i++){
+        //     for(int j=0;j<c;j++){
+        //       if(m[i][j]==0){
                 
-               for(int x=0;x<r;x++){
-                   for(int y=0;y<c;y++){
+        //        for(int x=0;x<r;x++){
+        //            for(int y=0;y<c;y++){
                      
-                     ans[i][y]=0;
+        //              ans[i][y]=0;
                  
-               }
-                ans[x][j]=0;
+        //        }
+        //         ans[x][j]=0;
                  
-               }
+        //        }
                  
                  
-                }
+        //         }
                  
-              }
-            }
+        //       }
+        //     }
 
         
-        m = ans;
+        // m = ans;
+
+
+        /////////////////brute force/////////////////
+
+    vector<bool>rowAr(row,false);
+    vector<bool>colAr(col,false);
+
+    for(int i=0;i<row;i++){
+        for(int j=0;j<col;j++){
+            if(m[i][j]==0){
+                rowAr[i]=true;
+                colAr[j]=true;
+            }
+        }
+    }
+
+    
+    for(int i=0;i<row;i++){
+        for(int j=0;j<col;j++){
+            if(rowAr[i]) m[i][j]=0;
+            if(colAr[j]) m[i][j]=0;
+        }
+    }
+
+
+
     }
 };
