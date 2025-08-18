ba91b5cca936ba370920036b087d2bbaf0d8a3c1
A. B. M. Shifar Emtiuz <90374362+ShifarEmtiuz07@users.noreply.github.com>
2025-08-18 16:24:51 +0600

    Time: 0 ms (100%), Space: 18.5 MB (78.73%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -39,24 +39,61 @@ public:
 
         /////////////////brute force/////////////////
 
-    vector<bool>rowAr(row,false);
-    vector<bool>colAr(col,false);
+    // vector<bool>rowAr(row,false);
+    // vector<bool>colAr(col,false);
 
-    for(int i=0;i<row;i++){
+    // for(int i=0;i<row;i++){
+    //     for(int j=0;j<col;j++){
+    //         if(m[i][j]==0){
+    //             rowAr[i]=true;
+    //             colAr[j]=true;
+    //         }
+    //     }
+    // }
+
+    
+    // for(int i=0;i<row;i++){
+    //     for(int j=0;j<col;j++){
+    //         if(rowAr[i]) m[i][j]=0;
+    //         if(colAr[j]) m[i][j]=0;
+    //     }
+    // }
+
+
+          /////////////////optimize/////////////////
+
+          bool rowZero=false;
+          bool colZero=false;
+          for(int i=0;i<row;i++){
+            if(m[i][0]==0) rowZero=true;
+          }
+             for(int i=0;i<col;i++){
+            if(m[0][i]==0) colZero=true;
+          }
+
+             for(int i=0;i<row;i++){
         for(int j=0;j<col;j++){
             if(m[i][j]==0){
-                rowAr[i]=true;
-                colAr[j]=true;
+             m[i][0]=0;
+              m[0][j]=0;
+
             }
         }
     }
 
-    
-    for(int i=0;i<row;i++){
-        for(int j=0;j<col;j++){
-            if(rowAr[i]) m[i][j]=0;
-            if(colAr[j]) m[i][j]=0;
+               for(int i=1;i<row;i++){
+        for(int j=1;j<col;j++){
+           if(m[i][0]==0) m[i][j]=0;
+            if(m[0][j]==0) m[i][j]=0;
+
         }
+    }
+
+    for(int i=0;i<row;i++){
+        if(rowZero) m[i][0]=0;
+    }
+        for(int i=0;i<col;i++){
+        if(colZero) m[0][i]=0;
     }
 
 
