26a9122def28cfc8b6d6b439f1fdada33032a814
A. B. M. Shifar Emtiuz <90374362+ShifarEmtiuz07@users.noreply.github.com>
2025-08-19 16:57:15 +0600

    Time: 0 ms (100%), Space: 9.8 MB (26.23%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,9 +1,29 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        if(s==t)return true;
-        else return false;
+
+        //////shortcut///// 
+
+        // sort(s.begin(),s.end());
+        // sort(t.begin(),t.end());
+        // if(s==t)return true;
+        // else return false;
+
+        /////better////
+
+        vector<int>sFreq(26,0);
+        vector<int>tFreq(26,0);
+        for(char c:s){
+            sFreq[c-'a']++;
+        }
+       for(char c:t){
+            tFreq[c-'a']++;
+        }
+
+        for(int i=0;i<26;i++){
+        if(sFreq[i]!=tFreq[i]) return false;
+        }
+        return true;
+        
     }
 };
